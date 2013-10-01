f7912e45ea7c2065dd7b67c43e5ef9c89fbb417d
denisovdenis <denisov@undev.ru>
2013-10-01 18:12:55 +0400

    getDupeWarning not static anymore

--- MultiUpload.body.php
+++ MultiUpload.body.php
@@ -351,7 +351,7 @@ class MultipleUpload extends SpecialUpload {
 	 * Construct a warning and a gallery from an array of duplicate files.
 	 * Override because the original doesn't say which file is a dupe
 	 */
-	public static function getDupeWarning( $dupes, $dupeTitle = null ) {
+	public function getDupeWarning( $dupes, $dupeTitle = null ) {
 		$result = parent::getDupeWarning( $dupes );
 		return preg_replace( '@<li>@', "<li>{$dupeTitle->getText()}", $result );
 	}
